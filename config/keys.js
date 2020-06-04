9a2f2adadae9d0564fd0c4ca32f9a4c985adf9b6
unknown <amannsiddhartha@gamil.com>
2020-06-04 22:56:00 +0530

    changing routes

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://aman:aman@cluster0-kot7e.mongodb.net/tuner?retryWrites=true&w=majority'}
+  module.exports = {mongoURI: 'mongodb+srv://aman:aman@cluster0-kot7e.mongodb.net/waves?retryWrites=true&w=majority'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost:27017/waves'}
 }
