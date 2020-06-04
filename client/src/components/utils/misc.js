72884856f4131374fd49a984a3ab372a8c66d326
unknown <amannsiddhartha@gamil.com>
2020-06-04 22:49:56 +0530

    changing routes

--- client/src/components/utils/misc.js
+++ client/src/components/utils/misc.js
@@ -1,4 +1,4 @@
 // SERVER Routes
 export const USER_SERVER = '/api/users';
-export const PRODUCT_SERVER = '/api/product/';
+export const PRODUCT_SERVER = '/api/product';
 export const SITE_SERVER = '/api/site';
